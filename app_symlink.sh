b353fd393d85cf52c25ebc9b78b5e60d7c3d01f4
Muhammad Nasrul <mnasruul@gmail.com>
2024-01-29 08:38:50 +0000

    oke

--- app_symlink.sh
+++ app_symlink.sh
@@ -1,6 +1,9 @@
-    #!/bin/bash
-    if ln -sf "/app/log/log_$(date +\%Y\%m\%d).txt" /app/log/log.log; then
-        echo "Success AppSymlink"
-    else
-        echo "Error: AppSymlink creation failed"
-    fi
+#!/bin/bash
+if ln -sf "/app/log/log_$(date +\%Y\%m\%d).txt" /app/log/log.log; then
+    echo "Success AppSymlink"
+else
+    echo "Error: AppSymlink creation failed"
+fi
+
+
+/opt/fluent-bit/bin/fluent-bit -c /etc/fluent-bit/fluent-bit.conf -vv &
