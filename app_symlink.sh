4c87fbbf9dbb1b0e279e68db43d14e8fbc2f070f
Muhammad Nasrul <mnasruul@gmail.com>
2024-01-29 06:26:43 +0000

    symlink

---
+++ app_symlink.sh
@@ -0,0 +1,6 @@
+    #!/bin/bash
+    if ln -sf "/app/log/log_$(date +\%Y\%m\%d).txt" /app/log/log.log; then
+        echo "Success AppSymlink"
+    else
+        echo "Error: AppSymlink creation failed"
+    fi
